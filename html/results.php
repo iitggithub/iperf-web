58700b5e38b814d30ca6deed193c37dad028236c
iitggithub <iitg@gmail.com>
2017-03-19 12:01:22 +1100

    removed redundant switch statement

--- html/results.php
+++ html/results.php
@@ -2,41 +2,31 @@
 
 $port_regex = '/[0-9]+/';
 
-$docker  = false;
-$prog    = (!empty($_REQUEST['prog'])) ? $_REQUEST['prog'] : NULL;
+$prog    = (!empty($_REQUEST['prog'])) ? $_REQUEST['prog'] : 'Docker';
 $version = (!empty($_REQUEST['version'])) ? $_REQUEST['version'] : 2;
 $params  = (!empty($_REQUEST['params'])) ? escapeshellcmd($_REQUEST['params']) : NULL;
 $target  = (!empty($_REQUEST['target'])) ? escapeshellcmd($_REQUEST['target']) : NULL;
 $port    = (preg_match($port_regex,$_REQUEST['port'])) ? $_REQUEST['port'] : 5001;
 $typeSW  = ($_REQUEST['type']=='udp') ? ' -u ' : ' ';
 
+if ($prog == 'Docker') {
+  $prog    = 'sudo /usr/local/bin/run20.py';
+  $args    = ($version == 2) ? '' : '-t 3.1.3';
+  $args   .= $typeSW . '-c ' . $target . ' -p ' . $port . ' ' . $params;
+  $command = $prog . ' --commands="' . $args . '"';
+} else {
+  $prog    = ($version == 2) ? 'iperf' : 'iperf3';
+  $args    = $typeSW . '-c ' . $target . ' -p ' . $port . ' ' . $params;
+  $command = $prog . $args;
+}
+
+// Not sure where i stole this code from... Once i remember, i'll link to it here
+
 $descriptorspec = array(
    0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
    1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
    2 => array("pipe", "w")    // stderr is a pipe that the child will write to
 );
-
-switch ($prog) {
-  case 'Iperf':
-    if ($version == 2) {
-      $prog = 'iperf';
-    } else {
-      $prog = 'iperf3';
-    }
-  break;
-  default:
-    $docker = true;
-    $prog   = 'sudo /usr/local/bin/run20.py';
-  break;
-}
-
-if ($docker) {
-  $args    = $typeSW . '-c ' . $target . ' -p ' . $port . ' ' . $params;
-  $command = $prog . ' --commands="' . $args . '"';
-} else {
-  $args    = $typeSW . '-c ' . $target . ' -p ' . $port . ' ' . $params;
-  $command = $prog . $args;
-}
 
 flush();
 $proc = proc_open($command, $descriptorspec, $pipes, realpath('./'), array());
