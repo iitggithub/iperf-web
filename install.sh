dbd5010d680a8351df001c476bc7815499cd9b19
iitggithub <iitg_mail@gmail.com>
2017-07-21 17:44:12 +1000

    updated installer after running it for the first time on a fresh amazon aws instance

--- install.sh
+++ install.sh
@@ -14,7 +14,8 @@ fi
 
 if [ ! -f "/usr/local/bin/docker-compose" ]
   then
-  sudo curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` >/usr/local/bin/docker-compose
+  curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` -o docker-compose
+  sudo mv docker-compose /usr/local/bin/docker-compose
   sudo chmod 755 /usr/local/bin/docker-compose
 fi
 
@@ -66,6 +67,8 @@ if [ -n "${SYSTEMCTL}" ]
   sudo ${SYSTEMCTL} start docker-iperf-web
   sudo ${SYSTEMCTL} enable docker-iperf-web
 else
+  sudo /etc/init.d/docker start
+  sudo chkconfig docker on
   echo "for i in tcp udp; do for p in `seq 5001 5005`; do cd /data/iperf-server-${i}-${p} && /usr/local/bin/docker-compose up -d; done; done" >>/etc/rc.local
   echo "cd /data/iperf-web && /usr/local/bin/docker-compose up -d" >>/etc/rc.local
   for i in tcp udp; do for p in `seq 5001 5005`; do cd /data/iperf-server-${i}-${p} && sudo /usr/local/bin/docker-compose up -d; done; done
